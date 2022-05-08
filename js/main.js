41ab2d5f4497dd008bb622d15e3a17a9d07f60c8
obordonado <obordonado@protonmail.com>
2022-05-08 16:58:53 +0200

    resolución de error

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 
-HEAD
-const sonido = new Audio(/audio/intro.mp3);
-music.play();
-music.loop=true;
-music.playbackRate=2;
-music.pause();
+
 const audio = new Audio();
 audio.src="./sound/inicio.mp3";
